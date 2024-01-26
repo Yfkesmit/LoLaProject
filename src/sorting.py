2ebff3da27882efb664fb450e9b7df047ecaa56f
Yfke Smit <smityfke@gmail.com>
2024-01-26 17:21:12 +0100

    subset with tree info

--- src/sorting.py
+++ src/sorting.py
@@ -1,31 +1,47 @@
+import spacy
 import numpy as np
 from datasets import Dataset
 from tqdm import tqdm
 
-def sort_on_premise_length(data):
-    sorted_indices = np.argsort([len(p) for p in tqdm(data['premise'], desc="Sorting on premise length")])
-    sorted_data = {
-        'premise': np.array(data['premise'])[sorted_indices],
-        'hypothesis': np.array(data['hypothesis'])[sorted_indices],
-        'label': np.array(data['label'])[sorted_indices],
-    }
-    return Dataset.from_dict(sorted_data)
+nlp = spacy.load('en_core_web_sm')
 
-def sort_on_hypothesis_length(data):
-    sorted_indices = np.argsort([len(h) for h in tqdm(data['hypothesis'], desc="Sorting on hypothesis length")])
-    sorted_data = {
-        'premise': np.array(data['premise'])[sorted_indices],
-        'hypothesis': np.array(data['hypothesis'])[sorted_indices],
-        'label': np.array(data['label'])[sorted_indices],
-    }
-    return Dataset.from_dict(sorted_data)
+# Jaccard similarity function
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union
 
-def sort_on_both_length(data):
-    both_lengths = [len(p) + len(h) for p, h in tqdm(zip(data['premise'], data['hypothesis']), desc="Sorting on both lengths")]
-    sorted_indices = np.argsort(both_lengths)
+def sort_on_pos_jaccard_similarity(data):
+    # list for [premise, hypothesis, label, Jaccard similarity]
+    prob = []
+
+    for i in tqdm(range(len(data['premise'])), desc="Sorting on POS Jaccard Similarity"):
+        premise = data['premise'][i]
+        hypothesis = data['hypothesis'][i]
+        label = data['label'][i]
+
+        # Finding the POS tags for the sentences
+        premise_doc = nlp(premise)
+        hypothesis_doc = nlp(hypothesis)
+
+        # Extracting the POS tags for premise and hypothesis
+        premise_pos_tags = set([token.pos_ for token in premise_doc])
+        hypothesis_pos_tags = set([token.pos_ for token in hypothesis_doc])
+
+        similarity = jaccard_similarity(premise_pos_tags, hypothesis_pos_tags)
+
+        prob.append((premise, hypothesis, label, similarity))
+
+    # Sorting the list in decreasing order based on the similarity's real value number
+    prob = sorted(prob, key=lambda x: x[3], reverse=True)
+
+    # The ordered list in ascending order based on the Jaccard similarity of POS tags
+    ordered_list = [(premise, hypothesis, label) for premise, hypothesis, label, _ in prob]
+
     sorted_data = {
-        'premise': np.array(data['premise'])[sorted_indices],
-        'hypothesis': np.array(data['hypothesis'])[sorted_indices],
-        'label': np.array(data['label'])[sorted_indices],
+        'premise': np.array([item[0] for item in ordered_list]),
+        'hypothesis': np.array([item[1] for item in ordered_list]),
+        'label': np.array([item[2] for item in ordered_list]),
     }
+
     return Dataset.from_dict(sorted_data)
