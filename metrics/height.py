66f586d7b8c70a43472f8fdb7a55a36dea64c817
Sarah Abdalla <131190836+sabdallaa@users.noreply.github.com>
2024-01-27 00:59:54 +0100

    cid -> pid

--- metrics/height.py
+++ metrics/height.py
@@ -10,24 +10,24 @@ def tree_height_metric(premise_tree_str, hypothesis_tree_str):
 
     return abs(height_premise - height_hypothesis)
 
-# list for [cid, premise_tree_str, hypothesis_tree_str, tree_height_difference]
+# list for [pid, premise_tree_str, hypothesis_tree_str, tree_height_difference]
 sorted_height = []
 
-# Currently done for 5 examples, but should be on whole subset
+# Currently done for 5 examples, but should be on the whole subset
 for i in range(5):
-    cid = subset["cid"][i]
+    pid = subset["pid"][i]
     premise_tree_str = subset["p_tree"][i]
     hypothesis_tree_str = subset["h_tree"][i]
 
     height_difference = tree_height_metric(premise_tree_str, hypothesis_tree_str)
 
-    sorted_height.append((cid, premise_tree_str, hypothesis_tree_str, height_difference))
+    sorted_height.append((pid, premise_tree_str, hypothesis_tree_str, height_difference))
 
 # Sorting the list in ascending order based on height difference's real value number
 # want a list in decreasing order? add reverse=True
 sorted_height = sorted(sorted_height, key=lambda x: x[3])
 # print(sorted_height)
 
-# List of the sorted 'cid' IF WANTED
-sorteid_height_cid = [cid for cid, _, _, _ in sorted_height]
-# print(sorteid_height_cid)
+# List of the sorted 'pid' IF WANTED
+sorteid_height_pid = [pid for pid, _, _, _ in sorted_height]
+# print(sorteid_height_pid)
