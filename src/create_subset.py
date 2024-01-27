5b0a6111acf2ace4564f0d8b9cd6812c5dcf3afd
Yfke Smit <smityfke@gmail.com>
2024-01-27 12:10:11 +0100

    added pid to snli subset

--- src/create_subset.py
+++ src/create_subset.py
@@ -1,38 +1,42 @@
+from collections import Counter
 from datasets import Dataset
 
 def split_into_subsets(data, subset_size=50001):
     subsets = []
-    for i in range(0, (len(data['premise'])+1), subset_size):
-        print(i)
-        subset_premise = data['premise'][i:i+subset_size]
-        subset_hypothesis = data['hypothesis'][i:i+subset_size]
-        subset_label = data['label'][i:i+subset_size]
-        index = 0
-        while index < len(subset_premise) - 2:
-            if subset_premise[index] == subset_premise[index + 1] == subset_premise[index + 2]:
-                index += 3  # Move to the next set of three items
-            else:
-                # Delete the first two items
-                del subset_premise[index:index + 2]
-                del subset_hypothesis[index:index + 2]  # Delete the first two items
-                del subset_label[index:index + 2]  # Delete the first two items
-        # Handle end of subset
-        if not (subset_premise[-1] == subset_premise[-2] == subset_premise[-3]):
-            if (subset_premise[-1] == subset_premise[-2]):
-                del subset_premise[-2:]
-                del subset_hypothesis[-2:]
-                del subset_label[-2:]
-            else:
-                del subset_premise[-1]
-                del subset_hypothesis[-1]
-                del subset_label[-1]
+    
+    # Convert lists to numpy arrays for faster indexing
+    premise_array = data['premise']
+    hypothesis_array = data['hypothesis']
+    label_array = data['label']
+    p_tree_array = data['p_tree']
+    h_tree_array = data['h_tree']
+    cid_array = data['cid']
+    pid_array = data['pid']
 
-        # Append the adjusted subset
+    for i in range(0, len(premise_array), subset_size):
+        print(i)
         subset = {
-            'premise': subset_premise,
-            'hypothesis': subset_hypothesis,
-            'label': subset_label
+            'premise': premise_array[i:i+subset_size],
+            'hypothesis': hypothesis_array[i:i+subset_size],
+            'label': label_array[i:i+subset_size],
+            'p_tree': p_tree_array[i:i+subset_size],
+            'h_tree': h_tree_array[i:i+subset_size],
+            'cid': cid_array[i:i+subset_size],
+            'pid': pid_array[i:i+subset_size]
         }
+
+        # Count occurrences of each 'cid' in the subset
+        cid_counter = Counter(subset['cid'])
+        
+        # Identify cids occurring at least 3 times
+        valid_cids = {cid for cid, count in cid_counter.items() if count % 3 == 0}
+        
+        # Filter subset to keep only rows with valid cids
+        valid_indices = [index for index, cid in enumerate(subset['cid']) if cid in valid_cids]
+
+        subset = {key: [subset[key][index] for index in valid_indices] for key in subset.keys()}
+        
+        # Append the adjusted subset
         subsets.append(Dataset.from_dict(subset))
 
     return subsets
