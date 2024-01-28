045a6b9624a27a62baaa8f7372a1f038d6950f52
TomasIken <tomas.iken@gmail.com>
2024-01-28 16:06:22 +0100

    added relative height metric

---
+++ metrics/realtive height.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from nltk import Tree
+import pickle
+import datasets
+def relative_height(path,h_rel = True):
+    df = pd.DataFrame(pd.read_pickle("datasets\snli_subset_with_trees.pkl"))
+
+    for index,row in df.iterrows():
+        df.loc[index,'p_rel'] = Tree.fromstring(df.loc[index]["p_tree"]).height()/len(df.loc[index]["premise"].split())
+        df.loc[index,'h_rel'] = Tree.fromstring(df.loc[index]["h_tree"]).height()/len(df.loc[index]["hypothesis"].split())
+    if h_rel == True:
+        df.sort_values(by=["h_rel"])
+    else:
+        df.sort_values(by=["p_rel"])
+
+    return datasets.Dataset.from_pandas(df)
+
+# with open('relative_h_height.pickle', 'wb') as handle:
+#     pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
