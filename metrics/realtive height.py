8ae8b1c9edf55b5a1b63c8baa821c8de0d17a448
TomasIken <tomas.iken@gmail.com>
2024-01-30 16:39:24 +0100

    added a relative pos height function

--- metrics/realtive height.py
+++ metrics/realtive height.py
@@ -15,5 +15,17 @@ def relative_height(path,h_rel = True):
 
     return datasets.Dataset.from_pandas(df)
 
+def relative_pos_height(path):
+    df = pd.DataFrame(pd.read_pickle(path))
+
+    for index,row in df.iterrows():
+        pos_height =  abs(Tree.fromstring(df.loc[index]["p_tree"]).height() - Tree.fromstring(df.loc[index]["h_tree"]).height())
+        pos_length = (len(df.loc[index]["premise"].split()) - len(df.loc[index]["hypothesis"].split()))
+        df.loc[index,'rel_pos_height'] = pos_height / pos_length
+        df.sort_values(by=["rel_pos_height"])
+
+
+    return datasets.Dataset.from_pandas(df)
+
 # with open('relative_h_height.pickle', 'wb') as handle:
 #     pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
